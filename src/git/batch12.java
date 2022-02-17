aad3d5fc1e50ea974727b266216079b30247cf36
NazgulBekova <dairovna.n@gmail.com>
2022-02-17 11:41:28 -0500

    I have updated some code

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,7 +5,7 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My birthday is coming!");
-
+		System.out.println("My birthday coming!");
 	}
 
 }
